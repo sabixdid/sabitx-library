c7ad8488e150fb37efcadecfa8fdf0363bd24989
sabixdid <boss@sabitinc.com>
2025-08-10 09:51:21 -0500

    1

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sabitx.app',
+  appName: 'SABITX',
+  webDir: 'www'
+};
+
+export default config;
